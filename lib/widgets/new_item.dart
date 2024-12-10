36c5134a1a30b0ac281dc4a243e7fe13fe30442e
Daniel Svirida <danielsvirida@gmail.com>
2024-12-09 17:06:20 -0800

    sending data to firebase

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -24,17 +26,30 @@ class _NewItemState extends State<NewItem> {
     if(_formKey.currentState!.validate()){
       _formKey.currentState!.save();
 
-      // final url = URI
-      // http.post();
-
-      Navigator.of(context).pop(
-          GroceryItem(
-            id: DateTime.now().toString(),
-            name: _enteredName,
-            quantity: _enteredQuantity,
-            category: _selectedCategory,
-          ),
+      final url = Uri.https(
+          'grocery-list-6bfea-default-rtdb.firebaseio.com',
+          'shopping-list.json'
       );
+      http.post(
+        url,
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: json.encode({
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory.title
+        })
+      );
+
+      // Navigator.of(context).pop(
+      //     GroceryItem(
+      //       id: DateTime.now().toString(),
+      //       name: _enteredName,
+      //       quantity: _enteredQuantity,
+      //       category: _selectedCategory,
+      //     ),
+      // );
     }
   }
 
